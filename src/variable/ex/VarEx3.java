d9b38ec0121d257a65dea6145b9bd410b7f6ac54
saki8661 <saki8661@naver.com>
2024-01-29 19:49:59 +0900

    변수, 연산자, 조건문

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long var = 10000000000L;
+        boolean q = true;
+
+        System.out.println(var);
+        System.out.println(q);
+    }
+}
